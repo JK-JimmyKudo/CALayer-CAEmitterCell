e73e88929f4d83223b770dba76a6cebe960bb05a
PW <565731968@qq.com>
2018-05-22 15:34:25 +0800

    增加霓虹效果和雪花效果

--- CALayer层动画详解/ViewController.m
+++ CALayer层动画详解/ViewController.m
@@ -116,11 +116,11 @@
         [self FlameEffect];
         
     }else if (indexPath.row == 1){
-        
+        [self emitterAnimate1];
     }else if (indexPath.row == 2){
-        
+        [self emitterAnimate];
     }else if (indexPath.row == 3){
-        
+        [self emitterAnimate3];
     }
 }
 
@@ -133,7 +133,7 @@
     2、velocity：表明当前粒子速度信息，不同的粒子速度控制燃烧的剧烈程度以及火焰的燃烧方向。
     */
     
-    self.view.backgroundColor = [UIColor blackColor];
+    self.tableView.backgroundColor = [UIColor blackColor];
 //    创建一个CAEmitterLayer,大小同view一样
     CAEmitterLayer *emitter = [CAEmitterLayer layer];
     emitter.frame = self.view.frame;
@@ -179,13 +179,143 @@
 }
 
 
+//鬼火效果
+-(void) emitterAnimate1{
+    self.tableView.backgroundColor = [UIColor blackColor];
+    CAEmitterCell *emitterCell = [CAEmitterCell emitterCell];
+    emitterCell.name = @"fire";
+    emitterCell.emissionLongitude = 0.8;
+    emitterCell.velocity = -1; //粒子速度 负数表明向上燃烧
+    emitterCell.velocityRange = 50; //粒子速度范围
+    emitterCell.emissionRange = 1.1; //周围发射角度
+    emitterCell.yAcceleration =  -200; //粒子y方向的加速度分量
+    emitterCell.scaleSpeed = 0.7; //缩放比例  超大火苗
+//    emitterCell.color = UIColor(red: 0 ,green: 0.4 ,blue:0.2 ,alpha:0.1).cgColor
+    emitterCell.color = [UIColor colorWithRed:0 green:0.4 blue:0.2 alpha:0.1].CGColor;
+//    emitterCell.contents = UIImage(named: "fire")!.cgImage
+    emitterCell.contents = (__bridge id )([UIImage imageNamed:@"fire"].CGImage);
+    
+    
+    
+    
+//    //每个图像单位
+//    CAEmitterCell *cell = [CAEmitterCell emitterCell];
+//    cell.contents = (__bridge id)[UIImage imageNamed:@"fire"].CGImage;
+//    cell.name = @"fire";//粒子的名字
+//    cell.emissionLongitude = 0.0;
+//    //    图像的出现频率（每秒钟图片出现的数量）
+//    cell.birthRate = 80;
+//    //每个图像的生命周期
+//    cell.lifetime = 3.0;
+//    //图片背景色，不设置就是原图
+//    cell.color = [UIColor yellowColor].CGColor;
+//    //透明度每过一秒就是减少0.5
+//    cell.alphaSpeed = -0.2;
+//    //发射速度
+//    cell.velocity = -1;
+//    //每个图像速度范围
+//    cell.velocityRange = 30;
+//    //散射的范围，目前是向四周
+//    cell.emissionRange = 1.1;
+//    //缩放比例  超大火苗
+//    cell.scaleSpeed = 0.3;
+//    //粒子y方向的加速度分量
+//    cell.yAcceleration = -40;
+//    //开始动画
+    
+    
+    
+    CAEmitterLayer *emitterLayer = [CAEmitterLayer layer];
+    emitterLayer.frame = self.view.frame;
+    emitterLayer.position = self.view.center; //粒子发射位置
+    emitterLayer.emitterPosition = CGPointMake(emitterLayer.frame.size.width / 2.0, emitterLayer.frame.size.height / 2.0);
+    [self.view.layer addSublayer:emitterLayer];
+
+//    emitterLayer.emitterSize = CGSize(width: 50, height: 10)//控制大小
+    emitterLayer.emitterSize = CGSizeMake(50, 10);
+    emitterLayer.renderMode = kCAEmitterLayerAdditive;
+    emitterLayer.emitterMode = kCAEmitterLayerOutline;
+    emitterLayer.emitterShape = kCAEmitterLayerLine;
+    emitterLayer.emitterCells = @[emitterCell];
+    
+    [emitterLayer setValue:@"500" forKey:@"emitterCells.fire.birthRate"];
+    [emitterLayer setValue:@"1" forKey:@"emitterCells.fire.lifetime"];
+    
+
+}
+
+//霓虹效果
+-(void) emitterAnimate{
+    self.tableView.backgroundColor = [UIColor blackColor];
+    CAEmitterCell *emitterCell = [CAEmitterCell emitterCell];
+    emitterCell.name = @"nenolight";
+    emitterCell.emissionLongitude  = 1.2;// emissionLongitude:x-y 平面的 发 射方向
+    emitterCell.velocity = 50;// 粒子速度
+    emitterCell.velocityRange = 50;// 粒子速度范围
+    emitterCell.scaleSpeed = -0.2;// 速度缩放比例 超大火苗
+    emitterCell.scale = 0.1;  //缩放比例
+    //R G B alpha 颜色速度渐变
+    emitterCell.greenSpeed = -0.1;
+    emitterCell.redSpeed = -0.2;
+    emitterCell.blueSpeed = 0.1;
+    emitterCell.alphaSpeed = -0.2;
+    emitterCell.birthRate = 100; // 一秒钟会产生粒子的个数
+    emitterCell.lifetime = 4; //粒子生命周期
+    emitterCell.color = [UIColor whiteColor].CGColor;
+//    emitterCell.contents = UIImage(named: "neonlight")!.cgImage
+    emitterCell.contents = (__bridge id )([UIImage imageNamed:@"nenolight"].CGImage);
+    
+    
+    CAEmitterLayer *emitterLayer = [CAEmitterLayer layer];
+    emitterLayer.position = self.view.center;// 粒子发射位置
+    emitterLayer.emitterSize = CGSizeMake(2, 2); // 控制粒子大小
+    emitterLayer.renderMode = kCAEmitterLayerBackToFront;
+    emitterLayer.emitterMode = kCAEmitterLayerOutline;// 控制发射源模式 即形状
+    emitterLayer.emitterShape = kCAEmitterLayerCircle;
+    emitterLayer.emitterCells = @[emitterCell];
+    [self.view.layer addSublayer:emitterLayer];
+}
 
 
 
-
-
-
-
+//雪花效果
+-(void)emitterAnimate3{
+    self.tableView.backgroundColor = [UIColor blackColor];
+    CAEmitterCell *emitterCell = [CAEmitterCell emitterCell];
+    emitterCell.name = @"snow";
+    emitterCell.birthRate = 150; // 每秒产生的粒子数量
+    emitterCell.lifetime = 5; //粒子的生命周期
+//    //yAcceleration 和 xAcceleration 分别表示颗粒在y和x方向上面运动的加速度
+    emitterCell.yAcceleration = 70.0;
+    emitterCell.xAcceleration = 10.0;
+    emitterCell.velocity = 20.0; //velocity表示粒子运动的速度
+    emitterCell.emissionLongitude = -M_PI_2;//表示颗粒初始时刻发射的方向(-M_PI_2 表示竖直往上发射)
+    emitterCell.velocityRange = 200;//加上这句代码之后，随机产生的速度范围为 -180(20 - 200) 到 220(20 + 200)
+    emitterCell.emissionRange = -M_PI_2 * 1.2;//我们可以将发射器的发射角度随机化
+//    //R G B alpha 颜色速度渐变
+//    emitterCell.color = UIColor(red: 0.9 ,green: 1.0 ,blue:1.0 ,alpha:1).cgColor
+    emitterCell.color = [UIColor colorWithRed:0.9 green:1.0 blue:1.0 alpha:1].CGColor;
+    emitterCell.greenSpeed = 0.3;
+    emitterCell.redSpeed = 0.3;
+    emitterCell.blueSpeed = 0.3;
+    emitterCell.scaleSpeed = -0.03;// 速度缩放比例
+    emitterCell.scale = 0.1;  //缩放比例
+    emitterCell.scaleRange = 0.1;
+//    emitterCell.contents = UIImage(named: "snow")!.cgImage
+    
+    emitterCell.contents = (__bridge id _Nullable)([UIImage imageNamed:@"snow"].CGImage);
+    
+    
+    CAEmitterLayer *emitterLayer = [CAEmitterLayer layer];
+//    //确定发射器的位置及尺寸
+//    emitterLayer.position = CGPoint(x: self.view.center.x, y: -70)
+    emitterLayer.position = CGPointMake(self.view.center.x, -70);
+//    emitterLayer.emitterSize = CGSize(width: UIScreen.main.bounds.size.width, height: 50)
+    emitterLayer.emitterSize = CGSizeMake(UIScreen.mainScreen.bounds.size.width, 50);
+        emitterLayer.emitterShape = kCAEmitterLayerRectangle;
+    emitterLayer.emitterCells = @[emitterCell];
+    [self.view.layer addSublayer:emitterLayer];
+}
 
 
 
